c12b8287685b762b7dbc8e0621a65d85bea8d8d0
Coy1944 <Samuelchincks86@gmail.com>
2022-06-12 11:18:20 -0600

    Updated Course Rating page

--- golfing/models.py
+++ golfing/models.py
@@ -34,6 +34,10 @@ class Courses(db.Model):
     course_location = db.Column(db.Integer, nullable=False)
     name = db.Column(db.Text, nullable=False)
 
+    def __repr__(self):
+        return f"Course('{self.name}', '{self.course_location}')"
+
+
 class Course_rating(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -52,33 +56,5 @@ class Post(db.Model):
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
 
-# class User(db.Model, UserMixin):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(20), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-#     password = db.Column(db.String(60), nullable=False)
-#     posts = db.relationship('Post', backref='author', lazy=True)
 
-#     def __repr__(self):
-#         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-# class Post(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(100), nullable=False)
-#     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-#     content = db.Column(db.Text, nullable=False)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-#     def __repr__(self):
-#         return f"Post('{self.title}', '{self.date_posted}')"
-
-
-# class Gcourse(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(200), nullable=False)
-#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-#     def __repr__(self):
-#         return '<name %r>' % self.id
 
